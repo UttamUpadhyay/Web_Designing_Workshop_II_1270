edbd20da97875a43ca3df9a361e94a3c77cd4f7a
UttamUpadhyay <upadhyayuttam197@gmail.com>
2026-05-18 00:45:53 +0530

    Experiment

--- Experiments/Experiment5/script.js
+++ Experiments/Experiment5/script.js
@@ -10,24 +10,19 @@ function addEmployee() {
         alert("Please fill all fields correctly!");
         return;
     }
-
     let employee = {
         name: name,
         id: id,
         salary: salary,
         dept: dept
     };
-
     employees.push(employee);
-
     alert("Employee Added Successfully!");
-
     document.getElementById("name").value = "";
     document.getElementById("id").value = "";
     document.getElementById("salary").value = "";
     document.getElementById("dept").value = "";
 }
-
 function displayEmployees() {
     let output = "<h3>All Employees</h3>";
 
@@ -37,7 +32,6 @@ function displayEmployees() {
 
     document.getElementById("output").innerHTML = output;
 }
-
 function filterSalary() {
     let filtered = employees.filter(emp => emp.salary > 50000);
 
@@ -49,14 +43,12 @@ function filterSalary() {
 
     document.getElementById("output").innerHTML = output;
 }
-
 function totalSalary() {
     let total = employees.reduce((sum, emp) => sum + emp.salary, 0);
 
     document.getElementById("output").innerHTML =
         `<h3>Total Salary Payout: ₹${total}</h3>`;
 }
-
 function avgSalary() {
     if (employees.length === 0) return;
 
@@ -66,7 +58,6 @@ function avgSalary() {
     document.getElementById("output").innerHTML =
         `<h3>Average Salary: ₹${avg.toFixed(2)}</h3>`;
 }
-
 function countByDept() {
     let deptCount = {};
 
